2e671d7fe5d8c936408832103ddcc8b66ad25e9f
Mike Pierce <info@michaelpierce.trade>
2020-11-09 14:56:39 +0100

    fix: Remove CSS variable insertion

--- postcss.config.js
+++ postcss.config.js
@@ -1,41 +1,7 @@
 const _ = require("lodash");
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
-const fs = require("fs");
-const path = require("path");
-const resolveConfig = require("tailwindcss/resolveConfig");
-const tailwindConfig = require("./tailwind.config.js");
 
 module.exports = {
-  plugins: [
-    function() {
-      let vars = ["colorList"],
-        tailwind = resolveConfig(tailwindConfig),
-        outputPath = path.join(__dirname, "src/styles/_$variables.scss"),
-        current = fs.readFileSync(outputPath).toString(),
-        content = "",
-        sassify = function(variable) {
-          let lines = [`$${variable}: (`];
-          let valueLines = [];
-
-          _.each(tailwind.theme.colors, function(value, key) {
-            valueLines.push(`  ${key}: ${value}`);
-          });
-          lines.push(_.join(valueLines, ",\n"));
-          lines.push(");\n");
-
-          return _.join(lines, "\n");
-        };
-
-      _.each(vars, function(value) {
-        content += sassify(value);
-      });
-
-      if (content !== current) {
-        fs.writeFileSync(outputPath, content);
-      }
-    },
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: [tailwindcss, autoprefixer],
 };
