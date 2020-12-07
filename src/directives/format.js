1bc81920aa29ca23dcb4f9a6f6c41452369ecb14
Mike Pierce <info@michaelpierce.trade>
2020-12-07 18:54:50 +0100

    Custom influence format

--- src/directives/format.js
+++ src/directives/format.js
@@ -1,7 +1,41 @@
-import { keys } from "lodash-es";
+import { endsWith, keys, reduce } from "lodash-es";
 import { chain } from "mathjs";
 import dayjs from "dayjs";
 import numeral from "numeral";
+
+numeral.register("format", "likes", {
+  regexps: {
+    format: /(\/likes)/,
+    unformat: /(\/likes)/,
+  },
+  format: (value, format, roundingFunction) => {
+    const space = endsWith(format, " /likes") ? " " : "";
+    const newFormat = format.replace(/\s?\/likes/, "");
+    const newValue = reduce(
+      { day: 7, hour: 24, min: 60, sec: 60 },
+      (accum, duration, label) => {
+        const check = accum.value / duration;
+        if (check > 1) {
+          accum = { value: check, label };
+        }
+
+        return accum;
+      },
+      { value, label: "week" }
+    );
+
+    const output = numeral._.numberToFormat(
+      newValue.value,
+      newFormat,
+      roundingFunction
+    );
+
+    return `${output}${space}/${newValue.label}`;
+  },
+  unformat: (string) => {
+    return numeral._.stringToNumber(string) * 0.01;
+  },
+});
 
 export default (el, { arg, value, modifiers }) => {
   const duration = (milliseconds) =>
@@ -21,11 +55,11 @@ export default (el, { arg, value, modifiers }) => {
       .multiply(1024)
       .format("0.00b");
 
-  const bandwidth = (bytes) => `${data(bytes)}/sec`;
+  const bandwidth = (bytes) => `${data(bytes)}`;
 
   const confidence = (trust) => `${numeral(trust).format("0.[00]a")}`;
 
-  const influence = (likes) => `${numeral(likes).format("0.[00]a")}/sec`;
+  const influence = (likes) => `${numeral(likes).format("0.[00]a/likes")}`;
 
   const formatters = {
     duration,
