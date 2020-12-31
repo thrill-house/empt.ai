da65791b8475c7f9275104b0ac4a4648d64d045b
Mike Pierce <info@michaelpierce.trade>
2020-12-31 12:32:54 +0100

    Describe

--- src/store/modules/__tests__/score.test.js
+++ src/store/modules/__tests__/score.test.js
@@ -2,41 +2,43 @@ import { extractValues } from "../score";
 import { takeRight } from "lodash-es";
 import transitions from "../__mocks__/transitions.mock.json";
 
-test("Handles empty state", () => {
-  const payload = { transitions: [], initial: {} };
-  const values = extractValues(payload);
+describe("score.extractValues", () => {
+  test("Handles empty state", () => {
+    const payload = { transitions: [], initial: {} };
+    const values = extractValues(payload);
 
-  expect(values).toStrictEqual({});
-});
-
-test("Calculates a single source", () => {
-  const payload = {
-    transitions: takeRight(transitions),
-    initial: {
-      bases: { influence: 0, bandwidth: 0 },
-      factors: { influence: 0, bandwidth: 0 },
-    },
-  };
-  const values = extractValues(payload);
-
-  expect(values).toStrictEqual({
-    bases: { influence: 1, bandwidth: 1 },
-    factors: { influence: 0, bandwidth: 0 },
+    expect(values).toStrictEqual({});
   });
-});
 
-test("Calculates a source and a slot with matching tree", () => {
-  const payload = {
-    transitions: takeRight(transitions, 2),
-    initial: {
-      bases: { influence: 0, bandwidth: 0 },
+  test("Calculates a single source", () => {
+    const payload = {
+      transitions: takeRight(transitions),
+      initial: {
+        bases: { influence: 0, bandwidth: 0 },
+        factors: { influence: 0, bandwidth: 0 },
+      },
+    };
+    const values = extractValues(payload);
+
+    expect(values).toStrictEqual({
+      bases: { influence: 1, bandwidth: 1 },
       factors: { influence: 0, bandwidth: 0 },
-    },
-  };
-  const values = extractValues(payload);
+    });
+  });
 
-  expect(values).toStrictEqual({
-    bases: { influence: 4, bandwidth: 2 },
-    factors: { influence: 0.2, bandwidth: 0.2 },
+  test("Calculates a source and a slot with matching tree", () => {
+    const payload = {
+      transitions: takeRight(transitions, 2),
+      initial: {
+        bases: { influence: 0, bandwidth: 0 },
+        factors: { influence: 0, bandwidth: 0 },
+      },
+    };
+    const values = extractValues(payload);
+
+    expect(values).toStrictEqual({
+      bases: { influence: 4, bandwidth: 2 },
+      factors: { influence: 0.2, bandwidth: 0.2 },
+    });
   });
 });
