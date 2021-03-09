033f6b362313cf9a7ccb75df1614e65077e81327
Mike Pierce <info@michaelpierce.trade>
2021-03-09 11:21:49 +0100

    Identity and contract script updates

--- .contract/scripts/registerContract.js
+++ .contract/scripts/registerContract.js
@@ -7,6 +7,9 @@ const clientOpts = {
   network: process.env.VUE_APP_GAME_NETWORK,
   wallet: {
     mnemonic: process.env.VUE_APP_GAME_MNEMONIC,
+    unsafeOptions: {
+      skipSynchronizationBeforeHeight: 415000, // only sync from start of 2021
+    },
   },
 };
 const client = new Dash.Client(clientOpts);
